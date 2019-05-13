2182390f3054aecb684d51d07952f4364b1947f5
williamgdo <williamgdo.app@gmail.com>
2019-05-13 17:31:42 -0300

    interface da tela de inicio modificada

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -2,23 +2,35 @@ import React from 'react'
 
 import './styles.css'
 
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/TextField';
+import NativeSelect from '@material-ui/core/NativeSelect';
+
 export default () => (
     <main className="Content page">
         <div className="intro-wrapper">
-            <p>Seja bem-vindo, vamos te auxiliar na escolha da sua universidade ! <br/> 
-            Preecha os campos a seguir para filtramos as faculdades com seu perfil :) <br/>
-            Campos com * (asterisco) são obrigatórios</p>
+            <p>Você pode preencher os campos abaixo para filtrarmos as faculdades de acordo com o seu perfil<br/> 
+                mas se preferir, pode pular as perguntas e ver a lista completa :) <br/></p>
         </div>
         <div className="form-wrapper">
             <form>
-                <label >Area desejada: </label>
-                <input type="text" name="area" className="input-area"></input>
-
-                <label >Estado de preferência: </label>
-                <input type="text" name="area" className="input-area"></input>
-
-                <label >Area desejada: </label>
-                <input type="text" name="area" className="input-area"></input>
+                {/* https://material-ui.com/demos/text-fields/ */}
+                <div>
+                    <label>Area desejada: </label>
+                    <Input id="area" name="area" className="input-area"></Input>
+                </div>
+                <div>
+                    <label>Estado de preferência: </label>
+                    <NativeSelect id="estado" name="area" className="input-area"></NativeSelect>
+                </div>
+                <div>
+                    <label>Area desejada: </label>
+                    <Input id="area2" name="area2" className="input-area2"></Input>
+                </div>
+                <div id="botoes">
+                    <Button id="pular" variant="contained" color="primary">Pular</Button>
+                    <Button id="aplicar" variant="contained" color="primary">Aplicar Filtros</Button>
+                </div>
             </form>
         </div>
     </main >
